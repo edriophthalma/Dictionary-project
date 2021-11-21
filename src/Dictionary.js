2e7b12b66ce542ec1f6cc95adb345c7e17a4834a
edriophthalma <86781480+edriophthalma@users.noreply.github.com>
2021-11-21 19:15:00 +0000

    axios and api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,19 +1,26 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary";
 
 export default function Dictionary() {
 
     const [word, searchWord] = useState("");
-
-    function search(event) {
-     event.preventDefault()
-     alert (`Looking for ${word}`);
+function wordSearch(response) {
+    console.log(response.data[0]);
     }
-function getWord(event) {
-    searchWord(event.target.value)
-   
+    function search(event) {
+     event.preventDefault();
+     
     
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+   
+     axios.get(apiUrl).then(wordSearch);
+     
 }
+function getWord(event) {
+    
+    searchWord(event.target.value); }
+
 
     return (<div>
         <form onSubmit={search}>
