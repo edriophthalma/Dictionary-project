bc991e0204063f2715998b405364e8162bafc987
edriophthalma <86781480+edriophthalma@users.noreply.github.com>
2021-11-23 17:05:14 +0000

    display synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -3,11 +3,17 @@ import Synonyms from "./Synonyms";
 export default function Meaning(props) {
  console.log(props.meaning);
  return (<div className="results">
-   
+     
    <h4>{props.meaning.partOfSpeech}</h4>
+   {props.meaning.definitions.map(function (definition, index) {
+       return (
+        <div key={index}> 
+   
      <p>Definition: {props.meaning.definitions[0].definition}</p>
      <p>Synonyms: <Synonyms synonyms={definition.synonyms} /></p>
 
     
+     </div>);})}
      </div>);
+     
 }
