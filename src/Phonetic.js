dd5d970c18d6089153dc5c96a300b744a24314ff
edriophthalma <86781480+edriophthalma@users.noreply.github.com>
2021-11-24 15:17:21 +0000

    removed warning noreferrer

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,7 +4,7 @@ export default function Phonetic(props) {
     console.log(props.phonetics);
     return  (
         <div className="listen">
-            <a href={props.phonetic.audio} target="_blank">
+            <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
                 Pronounciation
             </a> {props.phonetic.text}
         </div>);
